8404fc9f795556fafa734bfc0886c3ed4c0008ba
Brick <120332065+Bom1800contact@users.noreply.github.com>
2024-08-09 22:23:35 +0700

    Add files via upload

---
+++ assets/js/seach.js
@@ -0,0 +1,23 @@
+﻿// Lấy phần tử thanh tìm kiếm
+const searchInput = document.getElementById('searchText');
+
+// Lấy phần tử chứa các phần mềm trong Gaming Library
+const gamingLibraryItems = document.querySelectorAll('.gaming-library .item');
+
+// Thêm sự kiện khi người dùng nhập vào thanh tìm kiếm
+searchInput.addEventListener('keyup', () => {
+    const keyword = searchInput.value.toLowerCase(); // Lấy từ khóa và chuyển thành chữ thường
+
+    // Duyệt qua từng phần mềm trong thư viện game
+    gamingLibraryItems.forEach(item => {
+        const gameName = item.querySelector('h4').textContent.toLowerCase(); // Lấy tên game và chuyển thành chữ thường
+
+
+        // Kiểm tra xem tên game có chứa từ khóa hay không
+        if (gameName.includes(keyword)) {
+            item.style.display = 'block'; // Hiển thị phần mềm nếu có chứa từ khóa
+        } else {
+            item.style.display = 'none'; // Ẩn phần mềm nếu không chứa từ khóa
+        }
+    });
+});
